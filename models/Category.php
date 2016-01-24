357fed21137da522c16a63fe7f161758dc02b8fc
Joe Lumarlin <jlumarlin@gmail.com>
2016-01-24 22:15:15 +0200

    Work with navigation. Create Category.php

---
+++ models/Category.php
@@ -0,0 +1,28 @@
+<?php 
+
+include_once ROOT. '/components/DB.php';
+
+class Category{
+
+	public static function getCategoriesList(){
+		
+		$db = DB::getConnection();
+
+		$categoryList = array();
+
+		$result  = $db->query('SELECT id, name FROM category '
+		 						.'ORDER BY sort_order ASC');
+
+		$count = 0;
+		while ($row = $result->fetch()){
+			$categoryList[$count]['id'] = $row['id'];
+			$categoryList[$count]['name'] = $row['name'];
+			$count++;
+		}
+		return $categoryList;
+	}
+
+}
+
+
+?>
